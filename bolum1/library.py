8ce4e3f91f4636b9162d6a7df1bccbf58f2c8d4e
serracabaz34 <serracabaz7@gmail.com>
2025-08-19 16:29:11 +0300

    OOP Library

--- bolum1/library.py
+++ bolum1/library.py
@@ -21,7 +21,7 @@ class Library:
            self.books = []
 
     def save_books(self):
-        """save the books to json"""
+        """Save the books to json"""
         with open(self.filename, "w", encoding="utf-8") as f:
             json.dump([book.__dict__ for book in self.books], f,ensure_ascii = False, indent=4)
 
